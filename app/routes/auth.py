bf95d9004578091420b64b0e39f36f36e3c16918
Seu Nome <seu-email@exemplo.com>
2025-03-20 15:43:03 -0300

    Substituir werkzeug.urls.urlsplit por urllib.parse.urlsplit

--- app/routes/auth.py
+++ app/routes/auth.py
@@ -3,7 +3,7 @@ from flask_login import login_user, logout_user, login_required, current_user
 from app import db
 from app.models import User
 from app.forms import LoginForm, RegistrationForm, ProfileUpdateForm
-from werkzeug.urls import urlsplit
+from urllib.parse import urlsplit
 
 # Blueprint de autenticação
 auth_bp = Blueprint('auth', __name__)
