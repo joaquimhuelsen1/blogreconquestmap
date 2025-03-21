fc40c395ec24fb56edc21cdcb855cdf34e93b82b
Seu Nome <seu-email@exemplo.com>
2025-03-20 23:38:35 -0300

    Fix: Adiciona importação da função flash para o handler de erro CSRF

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, jsonify, render_template, session, g, redirect, url_for
+from flask import Flask, request, jsonify, render_template, session, g, redirect, url_for, flash
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 # Importar Flask-Migrate condicionalmente
