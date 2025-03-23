c93c1183637c10864e46dec207b7b36119abbacb
joaquimhuelsen1 <joaquimhuelsen@gmail.com>
2025-03-23 20:46:08 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,6 +4,5 @@ from app import create_app
 app = create_app()
 
 if __name__ == '__main__':
-    # Ponto de entrada principal da aplicação
-    print("Iniciando servidor na porta 5000...")
+    print("Iniciando servidor na porta 8000...")
     app.run(host='0.0.0.0', port=8000, debug=False)
